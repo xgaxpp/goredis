93e7b309e2559550a2784174f4356bd14d098873
稻谷 <gumingfeng@live.com>
2020-06-30 15:06:24 +0800

    12

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,27 +1,34 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/go-redis/redis"
+	"reflect"
 )
 
 func resolvingCmd(args []string) {
 	client := redisCli["cluster"]
-	if clusterCli, ok := (client).(redis.Client); ok {
+	object := reflect.ValueOf(client)
 
-	}
+	fmt.Println(object.NumMethod())
+
+	//v:= object.MethodByName("HGet")
+	//in := v.Type().NumIn()
+	//out := v.Type().NumOut()
+	//fmt.Println(in, out)
+	//var param []reflect.Value
+	////param[0] = reflect.Value{"", "", ""}
+	//fmt.Println(v.Call(param))
 }
 
 var redisCli = map[string]interface{}{
-	"cluster": *redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs:    []string{"123123", "123123"},
-		Password: "",
+	"cluster": redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: []string{},
 	}),
-	"single": *redis.NewClient(&redis.Options{
-		Addr:     "",
-		Password: "",
+	"single": redis.NewClient(&redis.Options{
+		Addr: "",
 	}),
-	"sentinel": *redis.NewSentinelClient(&redis.Options{
-		Addr:     "",
-		Password: "",
+	"sentinel": redis.NewSentinelClient(&redis.Options{
+		Addr: "",
 	}),
 }
